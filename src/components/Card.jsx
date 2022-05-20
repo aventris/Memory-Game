ab5849dcc2458f9cb72e3f6bf4427949f65a4ca6
aventris <ipo778@hotmail.com>
2022-05-20 10:29:34 -0500

    First commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "@styles/Card.scss";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card-front"></div>
+      <div className="card-back"></div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    port: 3005,
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    hot: true,
+    liveReload: true,
+  },
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: "/node_modules/",
+        use: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
